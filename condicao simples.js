4a89cccdeeec1ca78617d46fbb6eda5516b58a66
Hian Stafford <82848416+Hian-Stafford@users.noreply.github.com>
2021-09-02 11:02:18 -0300

    Create condicao simples.js

---
+++ condicao simples.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60)
+{
+    console.log(`Voce ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança e com cuidado!`)
